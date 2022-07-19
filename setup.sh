97f7453c0d5661a26b0ee92510de60fa1a921f89
Shivam Gupta <46683988+shivamgupta9@users.noreply.github.com>
2022-07-19 19:22:33 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+echo creating code coverage output file
+cat > ~/.aws/credentials << EOF
+=== Test Summary
+NAME                 VALUE                        
+───────────────────  ─────────────────────────────
+Outcome              Passed                       
+Tests Ran            678                          
+Pass Rate            100%                         
+Fail Rate            0%                           
+Skip Rate            0%                           
+Test Run Id          7070R000024h9Pr              
+Test Execution Time  868396 ms                    
+Org Id               00D0R000000lnGZUAY           
+Username             test-cjlnoqa1m1lt@example.com
+Org Wide Coverage    77%
+EOF
+echo Done
